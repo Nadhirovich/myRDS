549882adaaf4f4a1d4166a48df1c484d7e90b6d8
Nadir Metrouh <mnadir20@gmail.com>
2023-11-11 17:00:09 +0100

    enable backup and maintenance features in our RDS MySQL instance

--- main.tf
+++ main.tf
@@ -95,8 +95,19 @@ resource "aws_db_instance" "default" {
   db_subnet_group_name = aws_db_subnet_group.my_db_subnet_group.name
 
 
+  backup_retention_period = 7 # Number of days to retain automated backups
 
-  skip_final_snapshot = true
+  backup_window = "03:00-04:00" # Preferred UTC backup window (hh24:mi-hh24:mi format)
+
+  maintenance_window = "mon:04:00-mon:04:30" # Preferred UTC maintenance window
+
+  
+   # Enable automated backups
+
+  skip_final_snapshot = false
+
+  final_snapshot_identifier = "db-snap"
+
 
 }
 
